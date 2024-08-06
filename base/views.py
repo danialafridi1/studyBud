3ebd8580daa391de0aeb0d82e957abbf6f80b63e
Danial Afridi <iamdanialafridi@gmail.com>
2024-08-06 17:57:14 +0500

    feat: Add navbar and room template
    This commit adds the `navBar.html` template file, which contains the navigation bar for the website. It also adds the `room.html` template file, which displays the name of a room. These templates are used by the base app in the Django project.

--- base/views.py
+++ base/views.py
@@ -1,3 +1,40 @@
 from django.shortcuts import render
+from django.http import HttpResponse as HTTPResponse
+from .models import Room
+from .forms import RoomForm
+# rooms = [
+#     {
+#         'id':1,
+#      'name':'Lets learn python',
+#      },
+#     {
+#         'id':2,
+#         'name':'Lets learn django',
+#      },
+#     {
+#         'id':3,
+#         'name':'Lets learn react',
+#      },
+#     {
+#         'id':4,
+#         'name':'Lets learn angular',
+#      },
+#     {
+#         'id':5,
+#         'name':'Lets learn vue',
+#      },
+#   ]
+def home(request):
+    rooms = Room.objects.all()
+    context = {'rooms':rooms}
+    return render(request,'base/home.html', context)
 
-# Create your views here.
+def room(request,pk):
+    room = Room.objects.get(id=pk)  
+    context = {'room':room}
+    return render(request,'base/room.html',context)
+
+def createRoom(request):
+    form = RoomForm()
+    context = {'form' : form}
+    return render(request,'base/room_form.html',context)
