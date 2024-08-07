ead710b9d037238a2f04f5901b3fd08b97b9616c
Danial Afridi <iamdanialafridi@gmail.com>
2024-08-07 10:15:49 +0500

    feat: Update room display in home.html and add room update and delete functionality

--- base/urls.py
+++ base/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home,name='home'),
     path('room/<str:pk>/',views.room,name='room'),
-    path('create-room/',views.createRoom,name='create-room'),
+    path('create-room/',views.createRoom,name='room_form'),
+    path('update-room/<str:pk>/',views.updateRoom,name='update_room'),
+    path('delete-room/<str:pk>/',views.deleteRoom,name='delete_room'),
  ]
